75e8aea16e84827312a41a9238f4c7577db1683a
Nyawira Muturi <albinamuturi@gmail.com>
2020-04-21 12:56:48 +0300

    add encryption and decryption classes

---
+++ Encrypt.java
@@ -0,0 +1,17 @@
+package Encrypt;
+
+public class Encrypt{
+
+    public static String encrypt(String plainText, int shift) {
+        if(shift>26){
+            shift = shift%26;
+        }
+        else if(shift<0){
+            shift = (shift%26)+26;
+        }
+
+        String cipherText = "";
+        int length = plainText.length();
+        return cipherText;
+    }
+}
