b23bcae05fe3da6b3529fa072b9b6b1736e0c19f
Nyawira Muturi <albinamuturi@gmail.com>
2020-04-21 13:38:22 +0300

    add code to business logic to Encrypt.java

--- src/main/Encrypt.java
+++ src/main/Encrypt.java
@@ -12,6 +12,29 @@ public class Encrypt{
 
         String cipherText = "";
         int length = plainText.length();
+        for(int i = 0, i<length, i++){
+            char ch = plainText.charAt(i);
+            if(Character.isletter(ch)){
+                if(Character.isLowerCase(ch)){
+                    char c = (char)(ch+shift);
+                    if(c>'z'){
+                        cipherText += (char)(ch - (26-shift));
+                    }
+                    else{
+                        cipherText += c;
+                    }
+                }
+                else if(Character.isUpperCase(ch)){
+                    char c = (char)(ch+shift);
+                    if(c>'Z'){
+                        cipherText += (char)(ch - (26-shift));
+                    }
+                    else{
+                        cipherText += c;
+                    }
+                }
+            }
+        }
         return cipherText;
     }
 }
