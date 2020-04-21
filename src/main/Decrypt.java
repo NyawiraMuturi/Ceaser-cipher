b74a6fe76e8354d3af8ae2c9aa1b0bc7ec7a86c8
Nyawira Muturi <albinamuturi@gmail.com>
2020-04-21 13:50:42 +0300

    complete Decrypt.java code

--- src/main/Decrypt.java
+++ src/main/Decrypt.java
@@ -1,2 +1,43 @@
 package Decrypt;
 
+public class Decrypt{
+
+    public static String decrypt(String plainText, int shift) {
+        if(shift>26){
+            shift = shift%26;
+        }
+        else if(shift<0){
+            shift = (shift%26)+26;
+        }
+
+        String cipherText = "";
+        int length = plainText.length();
+        for(int i = 0, i<length, i++){
+            char ch = plainText.charAt(i);
+            if(Character.isletter(ch)){
+                if(Character.isLowerCase(ch)){
+                    char c = (char)(ch-shift);
+                    if(c<'a'){
+                        cipherText += (char)(ch + (26-shift));
+                    }
+                    else{
+                        cipherText += c;
+                    }
+                }
+                else if(Character.isUpperCase(ch)){
+                    char c = (char)(ch-shift);
+                    if(c<'A'){
+                        cipherText += (char)(ch + (26-shift));
+                    }
+                    else{
+                        cipherText += c;
+                    }
+                }
+            }
+            else {
+                cipherText += ch;
+            }
+        }
+        return cipherText;
+    }
+}
